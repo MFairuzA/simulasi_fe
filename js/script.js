afce701e99d3ccf98e1a2f783e5f3e407e2167b5
MFairuzA <fairuzab@gmail.com>
2023-12-03 12:05:44 +0700

    modified js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://rich-gold-elk-gear.cyclic.app';
 
 async function fetchBooks() {
   const bookList = document.getElementById('books-list');
